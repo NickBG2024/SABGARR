f940054f00306310164c7d57a41c1721abc7a0ed
Nick <nickgroll@gmail.com>
2024-09-23 12:59:39 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,16 +1,19 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 
+# Authenticate user
 def authenticate_user():
+    # Load credentials from Streamlit secrets
     credentials = {
         "usernames": {
             "admin_user": {
-                "name": st.secrets["credentials"]["usernames.admin_user.name"],
-                "password": st.secrets["credentials"]["usernames.admin_user.password"]
+                "name": st.secrets["admin"]["name"],
+                "password": st.secrets["admin"]["password"]
             }
         }
     }
 
+    # Use streamlit_authenticator to create an authenticator object
     authenticator = stauth.Authenticate(
         credentials,
         "myapp",
@@ -18,6 +21,7 @@ def authenticate_user():
         cookie_expiry_days=30
     )
 
+    # Call the authentication method
     name, authentication_status, username = authenticator.login("Login", "main")
 
     return authentication_status
