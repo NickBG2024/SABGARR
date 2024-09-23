0c2bf2dbd619331826ebb5f51126c18dd8fd6efc
Nick <nickgroll@gmail.com>
2024-09-23 12:32:51 +0200

    Update pwordhasher.py

--- pwordhasher.py
+++ pwordhasher.py
@@ -1,6 +1,11 @@
+import streamlit as st
 import bcrypt
 
-password = "rainyday"
+# Define your password
+password = "Chregan123@"
+
+# Hash the password
 hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
-print(f"Hashed password: {hashed_password}")
+# Display the hashed password using Streamlit
+st.write(f"Hashed password: {hashed_password}")
