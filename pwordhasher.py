ab2c100b9ed94d7b7d3a0c5f0633abc1b8337ab9
Nick <nickgroll@gmail.com>
2024-09-24 09:21:08 +0200

    Update pwordhasher.py

--- pwordhasher.py
+++ pwordhasher.py
@@ -11,4 +11,4 @@ hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 print("Hashed password:", hashed.decode())  # Ensure it's printed as a single string
 
 # Display the hashed password using Streamlit
-st.write(f"Hashed password: {hashed_password}")
+st.write(f"Hashed password: {hashed.decode()}")
