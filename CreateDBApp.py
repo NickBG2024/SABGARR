6cc870463cb2c6cc15c0c37f12da68dfe97710db
Nick <nickgroll@gmail.com>
2024-09-29 04:25:35 +0200

    Update CreateDBApp.py
    Changing to Match Results

--- CreateDBApp.py
+++ CreateDBApp.py
@@ -7,9 +7,9 @@ if st.button("Create Players Table"):
     create_players_table()
     st.success("Players table created.")
 
-if st.button("Create Matches Table"):
-    create_matches_table()
-    st.success("Matches table created.")
+if st.button("Create Match Results Table"):
+    create_match_results_table()
+    st.success("Match Results table created.")
 
 if st.button("Create Match Type Table"):
     create_match_type_table()
