87200431161fd787088a660343b052b16cd08f40
Nick <nickgroll@gmail.com>
2024-10-03 10:09:46 +0200

    Update PublicApp.py

--- PublicApp.py
+++ PublicApp.py
@@ -4,14 +4,22 @@ from database import get_standings, get_match_results, check_tables
 # Public-facing app for all users
 st.title("SABGA Backgammon: Round Robin 2025")
 
-st.sidebar.title("Public Section")
-page = st.sidebar.selectbox("View", ["Standings", "Fixtures", "Match History"])
+# Display tables in the sidebar
+tablecheck = check_tables()
 
-if page == "Standings":
+st.sidebar.title("Public Section")
+page = st.sidebar.selectbox("View", ["Leaderboard", "Fixtures", "Match History"])
+
+# Show Leaderboard
+if page == "Leaderboard":
     standings = get_standings()
     st.table(standings)
+
+# Show Fixtures
 elif page == "Fixtures":
-    st.write("Current fixtures:")
-elif page == "Match Results":
-    matchresults = get_match_results()
-    st.table(matchresults)
+    st.write("Fixtures will be listed here")
+
+# Show Match History
+elif page == "Match History":
+    match_results = get_match_results()
+    st.table(match_results)
