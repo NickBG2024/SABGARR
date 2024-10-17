3fd9fce0dcd4f2cad0c686f543c840a675290725
Nick <nickgroll@gmail.com>
2024-10-17 15:20:14 +0200

    Update PublicApp.py

--- PublicApp.py
+++ PublicApp.py
@@ -10,7 +10,7 @@ st.write("Welcome to the homepage of the South African Backgammon Round Robin! T
 
 # Check if the email checker is enabled
 if get_email_checker_status():
-    check_for_new_emails()  # Function that checks for new emails and parses them
+    #check_for_new_emails()  # Function that checks for new emails and parses them
 else:
     st.info("Email checker is currently disabled by the admin.")
     
