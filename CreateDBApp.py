11d76950b2d0a949444a6703b81c9f00bd4ae00c
Nick <nickgroll@gmail.com>
2024-10-29 21:16:01 +0200

    Update CreateDBApp.py

--- CreateDBApp.py
+++ CreateDBApp.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from database import create_players_table, create_match_results_table, create_match_type_table, create_appsettings_table, create_fixtures_table, alter_matchresults
+from database import create_players_table, create_series_table, create_match_results_table, create_match_type_table, create_appsettings_table, create_fixtures_table, alter_matchresults
 
 st.title("Create Backgammon Database")
 
@@ -7,6 +7,10 @@ if st.button("Create Players Table"):
     create_players_table()
     st.success("Players table created.")
 
+if st.button("Create Series Table"):
+    create_series_table()
+    st.success("Series table created.")
+    
 if st.button("Create Match Results Table"):
     create_match_results_table()
     st.success("Match Results table created.")
