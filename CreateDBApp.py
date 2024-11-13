a07e3be0ab75cabb51f8e61e2f6cb9015fe1dc7e
Nick <nickgroll@gmail.com>
2024-11-13 16:52:58 +0200

    Update CreateDBApp.py

--- CreateDBApp.py
+++ CreateDBApp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from database import get_crontest, crontest_table, empty_all_tables, reset_fixtures_completed, reset_match_results, print_table_structure, create_players_table, create_series_table, create_match_results_table, create_match_type_table, create_appsettings_table, create_fixtures_table
+from database import get_crontest2, crontest2_table, empty_all_tables, reset_fixtures_completed, reset_match_results, print_table_structure, create_players_table, create_series_table, create_match_results_table, create_match_type_table, create_appsettings_table, create_fixtures_table
 st.title("Create Backgammon Database")
 
 if st.button("Create Players Table"):
