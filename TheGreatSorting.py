00f058b06b279b7c5b43080691261e0cb04bf0ca
Nick <nickgroll@gmail.com>
2024-11-22 12:01:08 +0200

    Update TheGreatSorting.py

--- TheGreatSorting.py
+++ TheGreatSorting.py
@@ -30,7 +30,7 @@ with tab2:
 
         player_stats = get_player_stats_with_fixtures(1)
     
-         if player_stats:
+        if player_stats:
             formatted_stats = []
             for stat in player_stats:
                 name_with_nickname = f"{stat[1]} ({stat[2]})"
