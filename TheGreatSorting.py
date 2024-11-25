7e54ff4c66b1a49fed4a36cec5be8d96e3b06baa
Nick <nickgroll@gmail.com>
2024-11-25 08:56:33 +0200

    Update TheGreatSorting.py

--- TheGreatSorting.py
+++ TheGreatSorting.py
@@ -65,7 +65,7 @@ with tab2:
         display_match_grid(match_type_id)        
         list_remaining_fixtures(match_type_id)
         st.write("Maybe a metric of completion ?")
-    with  tab8:       
+    with tab8:       
         df = pd.DataFrame(
             {
                 "name": ["Roadmap", "Extras", "Issues"],
