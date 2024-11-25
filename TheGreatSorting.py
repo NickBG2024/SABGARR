ac02f475e3b15ae449420607b8c60b0722680af8
Nick <nickgroll@gmail.com>
2024-11-25 09:30:35 +0200

    Update TheGreatSorting.py

--- TheGreatSorting.py
+++ TheGreatSorting.py
@@ -78,15 +78,9 @@ with tab2:
             df,
             column_config={
                 "name": "App name",
-                "stars": st.column_config.NumberColumn(
-                    "Github Stars",
-                    help="Number of stars on GitHub",
-                    format="%d ⭐",
-                ),
-                "url": st.column_config.LinkColumn("App URL"),
-                "views_history": st.column_config.LineChartColumn(
-                    "Views (past 30 days)", y_min=0, y_max=5000
-                ),
+                "stars": "Stars",
+                "url": "ok",
+                "views_history": "ij"
             },
             hide_index=True,
         )
