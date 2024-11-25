61de2821ef40e2d1daf27b39224a040640d01478
Nick <nickgroll@gmail.com>
2024-11-25 09:33:38 +0200

    Update TheGreatSorting.py

--- TheGreatSorting.py
+++ TheGreatSorting.py
@@ -66,22 +66,9 @@ with tab2:
         list_remaining_fixtures(match_type_id)
         st.write("Maybe a metric of completion ?")
     with tab8:       
-        df = pd.DataFrame(
-            {
-                "name": ["Roadmap", "Extras", "Issues"],
-                "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
-                "stars": [random.randint(0, 1000) for _ in range(3)],
-                "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
-            }
-        )
-        st.dataframe(
-            df,
-            column_config={
-                "name": "App name",
-                "stars": "Stars",
-                "url": "ok",
-                "views_history": "ij"
-            },
-            hide_index=True,
-        )
-    
+       match_type_id = 6     
+        #Call function to show group table with match_type_id
+        display_group_table(match_type_id)
+        display_match_grid(match_type_id)        
+        list_remaining_fixtures(match_type_id)
+        st.write("Maybe a metric of completion ?")
