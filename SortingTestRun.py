238f56050d21588e35762b4f16f0c7d63273520c
Nick <nickgroll@gmail.com>
2024-12-07 20:58:49 +0200

    Update SortingTestRun.py

--- SortingTestRun.py
+++ SortingTestRun.py
@@ -30,6 +30,30 @@ with tab1:
     #Call function to show series table with series_id
     #display_series_table_completedonly(series_id)
     display_series_table(series_id)
+        df = pd.DataFrame(
+        {
+            "name": ["Roadmap", "Extras", "Issues"],
+            "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+            "stars": [random.randint(0, 1000) for _ in range(3)],
+            "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+        }
+    )
+    st.dataframe(
+        df,
+        column_config={
+            "name": "App name",
+            "stars": st.column_config.NumberColumn(
+                "Github Stars",
+                help="Number of stars on GitHub",
+                format="%d ⭐",
+            ),
+            "url": st.column_config.LinkColumn("App URL"),
+            "views_history": st.column_config.LineChartColumn(
+                "Views (past 30 days)", y_min=0, y_max=5000
+            ),
+        },
+        hide_index=True,
+    )
     show_matches_completed_by_series(series_id)
 with tab2:
     # Create tabs for additional stats
