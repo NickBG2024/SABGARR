4e72a7705ff64271b55a6196d487997efc334f86
Nick <nickgroll@gmail.com>
2025-01-01 19:33:37 +0200

    Update SortingTestRun.py

--- SortingTestRun.py
+++ SortingTestRun.py
@@ -4,7 +4,7 @@ import re
 import random
 import streamlit as st
 import pandas as pd
-from database import display_matchtype_standings_with_points, smccc, display_series_with_points, get_unique_player_count_by_series, get_matchcount_by_series, get_fixturescount_by_series, show_matches_completed_by_series, show_matches_completed, display_sorting_series_table, display_series_table, display_series_table_completedonly, display_match_grid, list_remaining_fixtures, display_group_table, get_remaining_fixtures, get_match_results_for_grid, get_player_stats_with_fixtures, get_player_stats_by_matchtype, get_sorting_standings, get_fixtures_with_names_by_match_type, get_match_results_nicely_formatted, print_table_structure, get_player_id_by_nickname, get_match_type_id_by_identifier, check_result_exists, insert_match_result, get_fixture, get_standings, get_match_results, check_tables, create_connection, insert_match_result, check_result_exists, get_email_checker_status 
+from database import display_series_standings_with_points, display_matchtype_standings_with_points, smccc, display_series_with_points, get_unique_player_count_by_series, get_matchcount_by_series, get_fixturescount_by_series, show_matches_completed_by_series, show_matches_completed, display_sorting_series_table, display_series_table, display_series_table_completedonly, display_match_grid, list_remaining_fixtures, display_group_table, get_remaining_fixtures, get_match_results_for_grid, get_player_stats_with_fixtures, get_player_stats_by_matchtype, get_sorting_standings, get_fixtures_with_names_by_match_type, get_match_results_nicely_formatted, print_table_structure, get_player_id_by_nickname, get_match_type_id_by_identifier, check_result_exists, insert_match_result, get_fixture, get_standings, get_match_results, check_tables, create_connection, insert_match_result, check_result_exists, get_email_checker_status 
 from datetime import datetime, timedelta, timezone
 
 # Add a header image at the top of the page
@@ -75,14 +75,8 @@ with tab3:
     tab11, tab12, tab13, tab14, tab15, tab16, tab17 = st.tabs(["Group 8","Group 9","Group 10","Group 11","Group 12","Group 13","Group 14"])
     
     with tab4:
-        # Example match type id
-        match_type_id = 4
-        #Call function to show group table with match_type_id
-        #display_group_metrics(match_type_id)
-        display_group_table(match_type_id)
-        display_match_grid(match_type_id)        
-        list_remaining_fixtures(match_type_id)
-        show_matches_completed(match_type_id)
+        def display_series_standings_with_points(series_id)
+        
     with tab5:
         match_type_id = 5      
         #Call function to show group table with match_type_id
