aa44c19a3621ae0521f3f25d5fd2c0545580d7f8
Nick <nickgroll@gmail.com>
2025-01-02 19:33:01 +0200

    Update database.py

--- database.py
+++ database.py
@@ -561,8 +561,8 @@ def display_match_grid(match_type_id):
         # Initialize an empty DataFrame with player names as both rows and columns
         score_df = pd.DataFrame(
             "–",  # Default value
-            index=player_names,
-            columns=player_names
+            index=player_names,  # Scoring players
+            columns=player_names  # Opponent players
         )
 
         # Populate the DataFrame with match results
@@ -573,9 +573,9 @@ def display_match_grid(match_type_id):
             player2_points = result[5]  # Points scored by Player 2
 
             if player1_points is not None:
-                score_df.at[player1_name, player2_name] = str(player1_points)  # Points scored by Player 1 against Player 2
+                score_df.at[player2_name, player1_name] = str(player1_points)  # Points Player 1 scored against Player 2
             if player2_points is not None:
-                score_df.at[player2_name, player1_name] = str(player2_points)  # Points scored by Player 2 against Player 1
+                score_df.at[player1_name, player2_name] = str(player2_points)  # Points Player 2 scored against Player 1
 
         def highlight_diagonal(df):
             # Create a blank style DataFrame with the same shape as the input DataFrame
