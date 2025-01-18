96781b96155ab9d28fb62525777e6d164c86da64
Nick <nickgroll@gmail.com>
2025-01-18 19:19:14 +0200

    Update 2025League.py

--- 2025League.py
+++ 2025League.py
@@ -55,6 +55,16 @@ with tab1:
 with tab2:
        # Example match type id
         match_type_id = 19
+        league_matches_played = get_matchcount_by_matchtype(matchtype_id)
+        league_fixtures = get_fixturescount_by_matchtype(matchtype_id)
+        if league_fixtures !=0:
+            percentage = (league_matches_played / league_fixtures) * 100
+            metric_value = f"{league_matches_played}/{league_fixtures} ({percentage:.1f}%)"
+        else:
+            percentage = 0
+            metric_value = f"{league_matches_played}/{league_fixtures} ({percentage:.1f}%)"
+
+        st.metric("A-League progress:",metric_value)
         #Call function to show group table with match_type_id
         display_matchtype_standings_with_points(match_type_id)
         #display_group_metrics(match_type_id)
