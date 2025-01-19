0dd253cbe8f0e95c1836109bb9c88923aa765c2a
Nick <nickgroll@gmail.com>
2025-01-19 09:47:18 +0200

    Update 2025League.py

--- 2025League.py
+++ 2025League.py
@@ -4,7 +4,7 @@ import re
 import random
 import streamlit as st
 import pandas as pd
-from database import get_fixturescount_by_matchtype, get_matchcount_by_matchtype, display_series_standings_with_points, display_matchtype_standings_with_points, get_matchcount_by_date_and_series, smccc, get_matchcount_by_series, get_fixturescount_by_series, show_matches_completed_by_series, show_matches_completed, display_sorting_series_table, display_series_table, display_series_table_completedonly, display_match_grid, list_remaining_fixtures, display_group_table, get_remaining_fixtures, get_match_results_for_grid, get_player_stats_with_fixtures, get_player_stats_by_matchtype, get_sorting_standings, get_fixtures_with_names_by_match_type, get_match_results_nicely_formatted, print_table_structure, get_player_id_by_nickname, get_match_type_id_by_identifier, check_result_exists, insert_match_result, get_fixture, get_standings, get_match_results, check_tables, create_connection, insert_match_result, check_result_exists, get_email_checker_status 
+from database import get_averagePR_by_matchtype, get_fixturescount_by_matchtype, get_matchcount_by_matchtype, display_series_standings_with_points, display_matchtype_standings_with_points, get_matchcount_by_date_and_series, smccc, get_matchcount_by_series, get_fixturescount_by_series, show_matches_completed_by_series, show_matches_completed, display_sorting_series_table, display_series_table, display_series_table_completedonly, display_match_grid, list_remaining_fixtures, display_group_table, get_remaining_fixtures, get_match_results_for_grid, get_player_stats_with_fixtures, get_player_stats_by_matchtype, get_sorting_standings, get_fixtures_with_names_by_match_type, get_match_results_nicely_formatted, print_table_structure, get_player_id_by_nickname, get_match_type_id_by_identifier, check_result_exists, insert_match_result, get_fixture, get_standings, get_match_results, check_tables, create_connection, insert_match_result, check_result_exists, get_email_checker_status 
 from datetime import datetime, timedelta, timezone, date
 
 # Add a header image at the top of the page
@@ -57,6 +57,7 @@ with tab2:
         match_type_id = 19
         league_matches_played = get_matchcount_by_matchtype(match_type_id)
         league_fixtures = get_fixturescount_by_matchtype(match_type_id)
+        ave_pr = get_averagePR_by_matchtype(match_type_id)
         
         if league_fixtures != 0:
             percentage = (league_matches_played / league_fixtures) * 100
@@ -74,10 +75,11 @@ with tab2:
         days_left = (end_date - today).days
         
         # Display metrics
-        col1, col2, col3 = st.columns(3)
+        col1, col2, col3, col4 = st.columns(3)
         col1.metric("A-League progress:", metric_value)
         col2.metric("Games remaining:", games_left)
         col3.metric("Days left:", days_left)
+        col4.metric("Average PR:", ave_pr)
         #Call function to show group table with match_type_id
         display_matchtype_standings_with_points(match_type_id)
         #display_group_metrics(match_type_id)
