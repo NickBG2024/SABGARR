c730168bc06b574c1fc59d28d266e9809eded4bc
Nick <nickgroll@gmail.com>
2025-01-28 06:34:49 +0200

    Update AdminOnly.py

--- AdminOnly.py
+++ AdminOnly.py
@@ -140,7 +140,7 @@ if see_series_details:
                     total = get_fixturescount_by_matchtype(match_type_id)
                     remaining = total - played
                     if played != 0:
-                        percentage = (total / played) * 100
+                        percentage = (total / played)
                     else : 
                         percentage = 0
                     metval = f"{played}/{total} ({percentage:.1f}%)"
