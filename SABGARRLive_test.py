a0c2a110c2f9f37210fe98bf6ead6ac38bda43ee
Nick <nickgroll@gmail.com>
2025-06-26 22:27:21 +0200

    Update SABGARRLive_test.py

--- SABGARRLive_test.py
+++ SABGARRLive_test.py
@@ -50,17 +50,12 @@ def league_tab(matchtype_id,league_title):
         col2.metric("Games remaining:", games_left)
         col3.metric("Days left:", days_left)
         col4.metric("Average PR:", ave_pr)
-        #Call function to show group table with match_type_id
-        #display_matchtype_standings_with_points_and_details(matchtype_id)
+
         display_cached_matchtype_standings(matchtype_id)
-        #display_matchtype_standings_full_details_styled(matchtype_id)                                                  
-        #display_group_metrics(match_type_id)
-        #display_group_table(match_type_id)
         display_match_grid(matchtype_id)       
         list_cached_remaining_fixtures(matchtype_id)
-        list_remaining_fixtures(matchtype_id)
         show_cached_matches_completed(matchtype_id)
-        show_matches_completed(matchtype_id)
+
 
 #2025 - SERIES 2 LEAGUE DATA DISPLAY        
 if series_choice == "2025 - Series 2":
@@ -120,17 +115,9 @@ if series_choice == "2025 - Series 2":
         st.markdown(f"All league information (rules, etc) can be found here: [SABGA Round Robin Leagues 2025 - rules etc v5.1.pdf]({pdf_url})", unsafe_allow_html=True)
         st.write("This tab offers an overview: a table showing all players, recent results and remaining fixtures.")
         
-        #Call function to show series table with current_series_id
-        #display_series_table_completedonly(current_series_id)
-        #display_series_table(current_series_id)
-        #display_sorting_series_table(current_series_id)
         fetch_cached_series_standings(current_series_id)
-        #display_series_standings_with_points_and_details(current_series_id)
-        #get_completed_matches_for_series(current_series_id)
         show_cached_remaining_fixtures_by_series(current_series_id)
         get_series_completed_matches_detailed(current_series_id)
-        #smccc(current_series_id)
-        #show_matches_completed_by_series(current_series_id)
 
     # League tabs - dynamically call league_tab() with appropriate matchtype_id
     for i, league_name in enumerate(tab_names[1:], start=1):  # Skip "OVERVIEW"
@@ -195,16 +182,9 @@ elif series_choice == "2025 - Series 1":
         st.markdown(f"All league information (rules, etc) can be found here: [SABGA Round Robin Leagues 2025 - rules etc v4.1.pdf]({pdf_url})", unsafe_allow_html=True)
         #st.write("This tab will offer an overview of sorts, recent results, player averages, rules, links to other standings, resources?")
         
-        #Call function to show series table with current_series_id
-        #display_series_table_completedonly(current_series_id)
-        #display_series_table(current_series_id)
-        #display_sorting_series_table(current_series_id)
         fetch_cached_series_standings(current_series_id)
-        #display_series_standings_with_points_and_details(current_series_id)
-        smccc(current_series_id)
         show_cached_remaining_fixtures_by_series(current_series_id)
         get_series_completed_matches_detailed(current_series_id)
-        #show_matches_completed_by_series(current_series_id)
 
     # League tabs - dynamically call league_tab() with appropriate matchtype_id
     for i, league_name in enumerate(tab_names[1:], start=1):  # Skip "OVERVIEW"
